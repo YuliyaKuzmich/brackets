87a82bbfe1b067708d279063ba29541387974591
Yuliya <kuzmich.yuliya@gmail.com>
2023-08-29 15:43:52 +0200

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-module.exports = function check(str, bracketsConfig) {
+//module.exports = 
+function check(str, bracketsConfig) {
   // your solution
+ let stack = [];
+ for (let i = 0; i < str.length; i++) {
+    if (str[i] === '(' || str[i] === '[' || str[i] === '{' || str[i] === '|') {
+      stack.push(str[i]);
+    } else if (str.length > 0
+      && str[i] === ')' && stack[stack.length - 1] === ')' ||
+      && str[i] === ')' && stack[stack.length - 1] === ')' ||
+      && str[i] === ')' && stack[stack.length - 1] === ')' ||
+      && str[i] === ')' && stack[stack.length - 1] === ')' ||
+      )
+ }
+
 }
+
+
+([{||}])
