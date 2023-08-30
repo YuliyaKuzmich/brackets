dcc2abe8fd9530780e549c35588f4f682572c3ad
Yuliya <kuzmich.yuliya@gmail.com>
2023-08-30 12:15:28 +0200

    finish: find solution

--- src/index.js
+++ src/index.js
@@ -10,33 +10,35 @@ module.exports = function check(str, bracketsConfig) {
   bracketsConfig.forEach((element) => {
     if (element[0] === element[1]) {
       sameSymbol.push(element[0]);
-    } else {
-    opened.push(element[0]);
-    closed.push(element[1]);
     }
+      opened.push(element[0]);
+      closed.push(element[1]);
+    
 
   })
 
- let count = 0;
  let stack = [];
- console.log(opened)
- console.log(closed)
- for (let i = 0; i < str.length; i++) {
-    if (sameSymbol.includes(str[i])) {
-      count = 
-    }
-    if (opened.includes(str[i])) {
-      stack.push(str[i]);
-    } else if ( str[i].length > 0 && closed.includes(str[i]) && 
-        stack[stack.length-1] === opened[closed.indexOf(str[i])]) {
-      stack.pop();
-    }else {
-      return false
-    }
+ for (let char in str) {
+    console.log(str[char])
+    if ( closed.includes(str[char])) {
+      const topElement = stack[stack.length - 1]; 
+      if (sameSymbol.includes(str[char]) && topElement !== str[char]) {
+        stack.push(str[char]);
+        continue;
+      }
+      if (stack.length > 0 && topElement === opened[closed.indexOf(str[char])]) {
+        stack.pop();
+      } else {
+        console.log(`top: ${topElement}`)
+        console.log(`open: ${opened[closed.indexOf(str[char])]}`)
+        return false;
+      }
+
+  } else if (opened.includes(str[char])) {
+      stack.push(str[char]);
+    } 
 
  } 
  return stack.length === 0;
 
 }
-
-//7, 11,12, 15, 19, 20
